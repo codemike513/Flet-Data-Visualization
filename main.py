572a4f3aef468ce69b0b7c0c615c6f9c37d4ef43
Mihir Pesswani <pesswani.mihir@gmail.com>
2023-06-12 21:48:22 +0530

    added page

--- main.py
+++ main.py
@@ -5,6 +5,26 @@ def main(page: ft.Page):
     page.horizontal_alignment = "center"
     page.vertical_alignment = "center"
 
+    page.add(
+        ft.Column(
+            expand=True,
+            alignment="center",
+            horizontal_alignment="center",
+            controls=[
+                ft.Container(
+                    expand=1,
+                    border_radius=6,
+                    bgcolor=ft.colors.with_opacity(0.025, ft.colors.WHITE10),
+                    content=ft.Row(alignment="center", controls=[]),
+                ),
+                ft.Container(
+                    expand=4,
+                    border_radius=6,
+                    bgcolor=ft.colors.with_opacity(0.025, ft.colors.WHITE10),
+                ),
+            ]
+        )
+    )
 
     page.update()
 
